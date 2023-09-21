c13c5d30d7a427c43dbac8ff99c4d2ef4653bd50
Igor Rukaleev <575714470@mail.ru>
2023-09-21 19:04:31 +0300

    Initial Commit

---
+++ 2D Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2D Game
+//
+//  Created by Igor on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
