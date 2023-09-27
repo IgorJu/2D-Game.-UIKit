333aa74f7c659fb83a5fe90b5b1afb977f683d7c
Igor Rukaleev <575714470@mail.ru>
2023-09-27 15:44:29 +0300

    adding cloud and obstacle animations

--- 2D Game/ViewControllers/GameViewController.swift
+++ 2D Game/ViewControllers/GameViewController.swift
@@ -9,19 +9,94 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    
     @IBOutlet var airplaneView: UIImageView!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupGradient()
-        UIView.animate(withDuration: 5) {
-            self.airplaneView.frame.origin.y = self.view.frame.height - 50
-            
+        callObstacle()
+        
+    }
+    
+    @IBAction func leftButton(_ sender: Any) {
+        
+        UIView.animate(withDuration: 0.3, animations: {
+            self.airplaneView.frame.origin.x -= 40
+            self.view.layoutIfNeeded()
+        })
+        
+    }
+    
+    @IBAction func rightButton(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.airplaneView.frame.origin.x += 40
+            self.view.layoutIfNeeded()
+        })
+        
+    }
+    
+    func addNewImageView() {
+        let xObstacle = Int.random(in: 60...250)
+        let yObstacle = 0
+        let obstacle = UIImageView(image: UIImage(named: "plane-2"))
+        obstacle.frame = CGRect(x: xObstacle, y: yObstacle, width: 50, height: 50)
+        view.addSubview(obstacle)
+        
+        UIView.animate(withDuration: 3, animations: {
+            obstacle.frame = CGRect(x: obstacle.frame.origin.x, y: self.view.frame.height, width: 60, height: 60)
+        }, completion: { (finished) in
+            obstacle.removeFromSuperview()
+        })
+        
+        
+        let cloud = UIImageView(image: UIImage(named: "cloud"))
+        let xCloud = Int.random(in: 60...250)
+        cloud.frame = CGRect(x: xCloud, y: 30, width: 40, height: 40)
+        view.addSubview(cloud)
+        
+        UIView.animate(withDuration: 15, animations: {
+            cloud.frame = CGRect(x: cloud.frame.origin.x, y: self.view.frame.height, width: 40, height: 40)
+        }, completion: { (finished) in
+            cloud.removeFromSuperview()
+        })
+        
+        let cloudTwo = UIImageView(image: UIImage(named: "cloud"))
+        cloudTwo.frame = CGRect(x: Int.random(in: 60...250), y: 30, width: 40, height: 40)
+        view.addSubview(cloudTwo)
+        
+        UIView.animate(withDuration: 15, animations: {
+            cloudTwo.frame = CGRect(x: cloudTwo.frame.origin.x, y: self.view.frame.height, width: 40, height: 40)
+        }, completion: { (finished) in
+            cloudTwo.removeFromSuperview()
+        })
+
+    
+        
+        airplaneView.frame = CGRect(
+            x: airplaneView.frame.origin.x,
+            y: airplaneView.frame.origin.y,
+            width: airplaneView.frame.size.width,
+            height: airplaneView.frame.size.height
+        )
+    }
+    
+    
+    func callObstacle() {
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { timer in
+            self.addNewImageView()
         }
     }
     
     
-
-
 }
+
+
+
+
+
+
+
+
+
